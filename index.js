83224c0a8d5c83a84c61aeac60edd4f8f90493aa
Shahariar Refat <shahariarrefat@gmail.com>
2026-06-09 14:17:22 +0600

    day-1

---
+++ index.js
@@ -0,0 +1,19 @@
+// ---------type casting---------
+// let age = prompt("what is your age");
+// age = Number(age);
+
+// console.log(age);
+
+// ----------swaping value-------
+let a = 20;
+let b = 10;
+// a = a + b; // 30
+// b = a - b;
+
+[]=[]
+
+console.log("a =", a);
+console.log("b =", b);
+
+
+
